68517fa1ebd632d7fb9dd107ba43783fe0832d1a
Krishna Kumar <krishnakumar778187@gmail.com>
2022-12-02 14:51:21 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -11,22 +10,13 @@ port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.get('/',(req, res) => {
+  res.status(200).json({message: "hellow from the server"})
+})
 
 app.use('/users', require('./routes/userRoutes'))
 app.use('/vendors', require('./routes/vendorRoutes'))
 
-// Server frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-      )
-    );
-  } else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-  }
 
 app.use(errorHandler);
 
